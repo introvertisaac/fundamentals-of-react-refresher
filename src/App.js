50dd230a2ed1b54d7797e1304254c0b0cebc25de
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2023-10-10 13:36:10 +0300

    react jsx, props and conditional rendering

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+
+
+ 
+const Job = (props) => {
+  return(
+    <>
+    <h1>{props.salary}</h1>
+    <h1>{props.position}</h1>
+    <h1>{props.company}</h1>
+    
+    </>
+    
+  );
+    
+}
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Job salary={1000} position="senior dev" company="amazon" />
+     <Job salary={252362} position=" dev" company="rubytech" />
+     <Job salary={3754} position="swe" company="google" />
+     <Job />
+  
+    </div>
+
+
+  );
+
+  
+}
+
+export default App;
